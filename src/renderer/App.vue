1c58759b3fa2b754d93545013611e16555fd7264
Fatih Acet <acetfatih@gmail.com>
2018-02-18 22:25:17 +0100

    AppHeader component.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,39 +1,29 @@
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState } from 'vuex';
+import AppHeader from './components/AppHeader';
 
 export default {
   name: 'CoinTop',
+  components: {
+    AppHeader,
+    AddExchangeModal,
+  },
   computed: {
     ...mapState([
       'isLoading',
     ]),
-    ...mapActions([
-      'fetchData',
-    ]),
-  },
-  mounted() {
-    this.fetchData();
   },
 };
 </script>
 
 <template>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-  <header class="app-header mdl-layout__header">
-    <div class="mdl-layout__header-row">
-      <span class="mdl-layout__title">CoinTop</span>
-      <div class="mdl-layout-spacer"></div>
-      <nav class="mdl-navigation">
-        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-          Add New Exchange
-        </button>
-      </nav>
-    </div>
-  </header>
+  <app-header />
   <main class="mdl-layout__content">
     <div
       v-if="isLoading"
-      class="app-progress mdl-progress mdl-js-progress mdl-progress__indeterminate"></div>
+      class="app-progress mdl-progress mdl-js-progress mdl-progress__indeterminate"
+    ></div>
     <router-view></router-view>
   </main>
 </div>
---
+++ src/renderer/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'AppHeader',
+  methods: {
+    ...mapActions([
+      'toggleAddExchangeModal',
+    ]),
+  },
+};
+</script>
+
+<template>
+  <header class="app-header mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <span class="mdl-layout__title">CoinTop</span>
+      <div class="mdl-layout-spacer"></div>
+      <nav class="mdl-navigation">
+        <button
+          @click="toggleAddExchangeModal"
+          class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+        >
+          Add Exchange
+        </button>
+      </nav>
+    </div>
+  </header>
+</template>
